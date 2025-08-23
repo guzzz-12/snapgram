c7a8d90bc6d50eacf7427b8fd3a4cb55feef4189
unknown <jegq.dev.projects@gmail.com>
2025-08-23 05:48:19 -0400

    Implementación de Clerk

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,23 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { ClerkProvider } from "@clerk/clerk-react";
+import { esMX } from "@clerk/localizations";
 import App from "./App.tsx";
 import "./index.css";
 
-const MyApp = () => {
-  return (
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error("Publishable key de Clerk no encontrado");
+}
+
+createRoot(document.getElementById("root")!).render(
+  <ClerkProvider
+    publishableKey={PUBLISHABLE_KEY}
+    localization={esMX}
+  >
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  )
-}
-
-createRoot(document.getElementById("root")!).render(<MyApp />);
+  </ClerkProvider>
+);

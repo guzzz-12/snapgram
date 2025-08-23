191fc295ca83189860720b5aade84dc3adf39565
unknown <jegq.dev.projects@gmail.com>
2025-08-23 05:49:51 -0400

    Implementación de routing con react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,92 @@
-import { Button } from "./components/ui/button"
+import { Route, Routes } from "react-router";
+import { useUser } from "@clerk/clerk-react";
+import AuthLayout from "@/pages/AuthLayout";
+import Layout from "@/pages/Layout";
+import HomePage from "@/pages/HomePage";
+import ChatBoxPage from "@/pages/ChatBoxPage";
+import ConnectionsPage from "@/pages/ConnectionsPage";
+import CreatePostPage from "@/pages/CreatePostPage";
+import DiscoverPage from "@/pages/DiscoverPage";
+import FeedPage from "@/pages/FeedPage";
+import ProfilePage from "@/pages/ProfilePage";
+import LoginPage from "@/pages/LoginPage";
+import SignupPage from "@/pages/SignupPage";
+import NoAuthRoute from "./components/NoAuthRoute";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 const App = () => {
+  const {isSignedIn} = useUser();
+
   return (
-    <div>
-      <Button className="cursor-pointer">
-        Bienvenidos a SnapGram
-      </Button>
-    </div>
+    <Routes>
+      <Route path="/" element={!isSignedIn ? <AuthLayout /> : <Layout />}>
+        <Route index element={!isSignedIn ? <LoginPage /> : <HomePage />} />
+        <Route
+          path="login"
+          element={
+            <NoAuthRoute>
+              <LoginPage />
+            </NoAuthRoute>
+          }
+        />
+        <Route
+          path="signup"
+          element={
+            <NoAuthRoute>
+              <SignupPage />
+            </NoAuthRoute>
+          }
+        />
+        <Route
+          path="connections"
+          element={
+            <ProtectedRoute>
+              <ConnectionsPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="create-post"
+          element={
+            <ProtectedRoute>
+              <CreatePostPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="discover"
+          element={
+            <ProtectedRoute>
+              <DiscoverPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="feed"
+          element={
+            <ProtectedRoute>
+              <FeedPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="messages/:userId"
+          element={
+            <ProtectedRoute>
+              <ChatBoxPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="profile"
+          element={
+            <ProtectedRoute>
+              <ProfilePage />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
+    </Routes>
   )
 }
 

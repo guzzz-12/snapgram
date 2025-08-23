0bc2de1201d783142f1254d0af34c94fbb50d590
unknown <jegq.dev.projects@gmail.com>
2025-08-23 09:27:33 -0400

    Layout principal de los usuarios autenticados

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,6 +1,25 @@
+import { Outlet } from "react-router";
+import { useUser } from "@clerk/clerk-react";
+import MainSidebar from "@/components/MainSidebar";
+import { SidebarProvider } from "@/components/ui/sidebar";
+
 const Layout = () => {
+  const {isLoaded} = useUser();
+
+  if (!isLoaded) {
+    return null;
+  }
+
   return (
-    <div>Layout</div>
+    <SidebarProvider>
+      <div className="flex w-full min-h-screen">
+        <MainSidebar />
+
+        <section className="grow bg-slate-50">
+          <Outlet />
+        </section>
+      </div>
+    </SidebarProvider>
   )
 }
 

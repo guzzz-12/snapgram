915c266cc872292308dcdfcd7883b2af07867e8b
unknown <jegq.dev.projects@gmail.com>
2025-08-29 11:13:57 -0400

    Mostrar los posts en el home. Correcciones de estilos en el home

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,11 +1,25 @@
+import PostCard from "@/components/posts/PostCard";
 import StoriesSlider from "@/components/stories/StoriesSlider";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { dummyPostsData } from "@/dummy-data";
 
 const HomePage = () => {
   return (
-    <main className="relative w-full h-full px-14 pt-6">
-      <SidebarTrigger className="absolute top-4 left-1 mt-1 ml-1 cursor-pointer z-10" />
-      <StoriesSlider />
+    <main className="relative flex justify-between gap-12 w-full h-full px-14 pt-6 pb-9 bg-slate-200">
+      <SidebarTrigger className="absolute block md:hidden top-4 left-4 cursor-pointer z-10" />
+      <div className="w-full h-full shrink-[2]">
+        <StoriesSlider />
+        
+        <section className="flex flex-col gap-6 w-full">
+          {dummyPostsData.map((post) => (
+            <PostCard key={post._id} postData={post} />
+          ))}
+        </section>
+      </div>
+
+      <aside className="w-[320px] h-full shrink-[1] p-1 rounded-md shadow bg-neutral-100">
+        <p>Sección de sugerencias</p>
+      </aside>
     </main>
   )
 }

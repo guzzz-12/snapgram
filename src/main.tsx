2fa844e7d39a5f1537a7b629731f654b2120768c
unknown <jegq.dev.projects@gmail.com>
2025-09-24 09:00:43 -0400

    Implementación de tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { esMX } from "@clerk/localizations";
 import App from "./App.tsx";
@@ -11,13 +12,17 @@ if (!PUBLISHABLE_KEY) {
   throw new Error("Publishable key de Clerk no encontrado");
 }
 
+const queryClient = new QueryClient();
+
 createRoot(document.getElementById("root")!).render(
-  <ClerkProvider
-    publishableKey={PUBLISHABLE_KEY}
-    localization={esMX}
-  >
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ClerkProvider>
+  <QueryClientProvider client={queryClient}>
+    <ClerkProvider
+      publishableKey={PUBLISHABLE_KEY}
+      localization={esMX}
+    >
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ClerkProvider>
+  </QueryClientProvider>
 );

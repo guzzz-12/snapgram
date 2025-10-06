db07c757ddce6cfa2e4d83170686780941c56fa6
unknown <jegq.dev.projects@gmail.com>
2025-10-06 09:45:05 -0400

    Query de los posts del usuario (sin paginación), query de la data del usuario

--- src/pages/ProfilePage.tsx
+++ src/pages/ProfilePage.tsx
@@ -1,40 +1,88 @@
-import { useEffect, useState } from "react";
-import { useParams, Navigate } from "react-router";
+import { Navigate, useParams } from "react-router";
+import { useInfiniteQuery, useQuery } from "@tanstack/react-query";
+import { useAuth } from "@clerk/clerk-react";
+import { RotateCw, TriangleAlert } from "lucide-react";
 import { toast } from "sonner";
 import PostCard from "@/components/posts/PostCard";
 import ProfileHeader from "@/components/profile/ProfileHeader";
 import PostCardSkeleton from "@/components/posts/PostCardSkeleton";
 import ProfileHeaderSkeleton from "@/components/posts/ProfileHeaderSkeleton";
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import { dummyPostsData, dummyUsersData, type PostType, type UserType } from "@/dummy-data";
+import { Button } from "@/components/ui/button";
+import { axiosInstance } from "@/utils/axiosInstance";
+import type { PostType, UserType } from "@/types/global";
 
 const ProfilePage = () => {
-  const {userId} = useParams<{userId: string}>();
+  const {userClerkId} = useParams<{userClerkId: string}>();
+  const {getToken} = useAuth();
 
-  const [userData, setUserData] = useState<UserType | null>(null);
-  const [postsData, setPostsData] = useState<PostType[]>([]);
-  const [loadingUser, setLoadingUser] = useState(true);
-  const [loadingPosts, setLoadingPosts] = useState(true);
-  
-  useEffect(() => {
-    const user = dummyUsersData.find((user) => user._id === userId)!;
-    const posts = dummyPostsData.filter((post) => post.user._id === userId);
+  const getUser = async () => {
+    const token = await getToken();
 
-    setTimeout(() => {
-      setLoadingUser(false);
-      setUserData(user);
-    }, 1500);
+    const {data} = await axiosInstance<{data: UserType}>({
+      method: "GET",
+      url: `/users/${userClerkId}`,
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
 
-    setTimeout(() => {
-      setLoadingPosts(false);
-      setPostsData(posts);
-    }, 2500);
-  }, [userId]);
+    return data.data;
+  }
+
+  const getUserPosts = async (page: number) => {
+    const token = await getToken();
+    
+    const {data} = await axiosInstance<{
+      data: PostType[];
+      hasMore: boolean;
+      nextPage: number | null;
+    }>({
+      method: "GET",
+      url: `/posts/user/${userData?._id}`,
+      params: {
+        page,
+        limit: 5
+      },
+      headers: {
+        Authorization: `Bearer ${token}`
+      }
+    });
+
+    return data;
+  }
+
+  const {data: userData, isLoading: loadingUser, error: userError} = useQuery({
+    queryKey: ["user", userClerkId],
+    queryFn: getUser,
+    enabled: !!userClerkId
+  });
+
+  const {data, error, isLoading, isRefetching, isFetchingNextPage, hasNextPage, fetchNextPage, refetch} = useInfiniteQuery({
+    queryKey: ["posts", userClerkId],
+    queryFn: ({pageParam}) => getUserPosts(pageParam),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.hasMore ? lastPage.nextPage : null,
+    refetchOnWindowFocus: false,
+    enabled: !!userData,
+    retry: 2,
+  });
 
   if (!loadingUser && !userData) {
     toast.error("Usuario no encontrado.");
     return <Navigate to="/" replace />
   }
+
+  if (userError) {
+    toast.error("Error al obtener el usuario.");
+  }
+
+  if (error) {
+    toast.error("Error al obtener los posts.");
+  }
+
+  const postsData = data?.pages.flatMap((page) => page.data) ?? [];
+  const loadingPosts = isLoading || isFetchingNextPage;
 
   return (
     <main className="w-full min-h-screen mx-auto p-6 bg-slate-200">
@@ -51,6 +99,26 @@ const ProfilePage = () => {
       </section>
 
       <section className="flex flex-col gap-6 w-full max-w-[600px] mx-auto">
+        {!loadingPosts && error &&
+          <div className="flex flex-col justify-center items-center gap-4">
+            <div className="flex justify-center items-center gap-3">
+              <TriangleAlert className="size-9 shrink-0 text-orange-600" />
+              <p className="text-center text-lg text-neutral-700">
+                Error al obtener los posts del usuario.
+              </p>
+            </div>
+
+            <Button
+              className="text-white bg-[#4F39F6] hover:bg-[#331fcf] cursor-pointer"
+              disabled={isRefetching}
+              onClick={() => refetch()}
+            >
+              <RotateCw aria-hidden />
+              Intentar nuevamente
+            </Button>
+          </div>
+        }
+
         {loadingPosts &&
           Array(3).fill(0).map((_, i) => (
             <PostCardSkeleton key={i} />
@@ -63,7 +131,7 @@ const ProfilePage = () => {
           ))
         }
 
-        {!loadingPosts && postsData.length === 0 &&
+        {!loadingPosts && userData && postsData.length === 0 &&
           <p className="text-center text-lg text-neutral-700">
             No se encontraron publicaciones.
           </p>

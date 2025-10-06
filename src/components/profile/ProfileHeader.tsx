999b35ec86d5ac2bbc31b0751b8bea4f68d33dd1
unknown <jegq.dev.projects@gmail.com>
2025-10-06 09:42:39 -0400

    Funcionalidad para mostrar/ocultar el texto completo de la biografía

--- src/components/profile/ProfileHeader.tsx
+++ src/components/profile/ProfileHeader.tsx
@@ -1,10 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import { Calendar, MapPin, Pencil } from "lucide-react";
+import ProfileEditModal from "./ProfileEditModal";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { Button } from "../ui/button";
 import { Separator } from "../ui/separator";
-import type { UserType } from "@/dummy-data";
-import ProfileEditModal from "./ProfileEditModal";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import useClampedText from "@/hooks/useClampedText";
+import { cn } from "@/lib/utils";
+import type { UserType } from "@/types/global";
 
 interface Props {
   userData: UserType;
@@ -12,10 +15,21 @@ interface Props {
 
 const ProfileHeader = ({ userData }: Props) => {
   const avatarRef = useRef<HTMLDivElement | null>(null);
+  const textContentRef = useRef<HTMLParagraphElement>(null);
+  const showClampBtnRef = useRef<"shouldShow" | "shouldNotShow">("shouldNotShow");
 
   const [viewportWidth, setViewportWidth] = useState(window.innerWidth);
   const [avatarHeight, setAvatarHeight] = useState(0);
   const [openEditModal, setOpenEditModal] = useState(false);
+
+  const {user} = useCurrentUser();
+
+  const {
+    isClamped,
+    showFullText,
+    setShowFullText,
+    setIsClamped
+  } = useClampedText({ textContentRef, showClampBtnRef, clampedText: userData.bio });
 
   useEffect(() => {
     if (avatarRef.current) {
@@ -35,7 +49,7 @@ const ProfileHeader = ({ userData }: Props) => {
   }, []);
 
   return (
-    <div className="block min-[850px]:grid grid-cols-1 grid-rows-5 w-full rounded-lg bg-white shadow overflow-hidden">
+    <div className="block rounded-lg bg-slate-50 shadow overflow-hidden">
       <ProfileEditModal
         userData={userData}
         isOpen={openEditModal}
@@ -44,23 +58,26 @@ const ProfileHeader = ({ userData }: Props) => {
 
       <div
         style={{
-          backgroundImage: `url(${userData.cover_photo})`,
+          backgroundImage: `url(${userData.coverPhoto || "/placeholder_image.webp"})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat"
         }}
-        className="w-full h-[180px] min-[850px]:h-[auto] col-span-1 row-span-2 bg-linear-to-r from-[#c8d3ff] to-[#fccee9]"
+        className="w-full h-[200px] shadow-sm g-linear-to-r from-[#c8d3ff] to-[#fccee9]"
       />
 
-      <div className="relative flex flex-col min-[850px]:flex-row w-full col-span-1 row-span-3">
+      <div className="relative flex flex-col min-[850px]:flex-row w-full">
         <div className="absolute translate-y-[-50%] min-[850px]:static flex items-start shrink-0 pl-6 min-[850px]:translate-y-[0]">
           <Avatar
             ref={avatarRef}
             className="w-[120px] h-[120px] min-[850px]:w-[100px] min-[850px]:h-[100px] min-[950px]:w-[120px] min-[950px]:h-[120px] shrink-0 min-[850px]:translate-y-[-50%] outline-4 outline-white"
           >
-            <AvatarImage src={userData.profile_picture} />
+            <AvatarImage
+              className="w-full h-full object-cover"
+              src={userData.profilePicture || "/default_avatar.webp"}
+            />
             <AvatarFallback>
-              {userData.full_name.charAt(0).toUpperCase()}
+              {userData.fullName.charAt(0).toUpperCase()}
             </AvatarFallback>
           </Avatar>
         </div>
@@ -74,7 +91,7 @@ const ProfileHeader = ({ userData }: Props) => {
           <div className="flex justify-between items-center mb-4">
             <div className="grow overflow-hidden">
               <h1 className="mb-0 text-2xl font-semibold truncate">
-                {userData.full_name}
+                {userData.fullName}
               </h1>
 
               <span className="text-neutral-700 truncate">
@@ -82,27 +99,49 @@ const ProfileHeader = ({ userData }: Props) => {
               </span>
             </div>
 
-            <Button
-              className="shrink-0 cursor-pointer"
-              variant="outline"
-              onClick={() => setOpenEditModal(true)}
-            >
-              <Pencil />
-              <span>Editar</span>
-            </Button>
+            {user?._id === userData._id &&
+              <Button
+                className="shrink-0 cursor-pointer"
+                variant="outline"
+                onClick={() => setOpenEditModal(true)}
+              >
+                <Pencil />
+                <span>Editar</span>
+              </Button>
+            }
           </div>
 
-          <p className="mb-4 text-neutral-700">
-            {userData.bio}
-          </p>
+          <div className="max-h-[450px] mb-4 overflow-y-auto scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+            <p
+              ref={textContentRef}
+              className={cn("inline-block text-left text-neutral-700 whitespace-pre-wrap", showFullText ? "line-clamp-none" : "line-clamp-3")}
+            >
+              {userData.bio}
+            </p>
+
+            {showClampBtnRef.current === "shouldShow" &&
+              <Button
+                className="inline-block my-1 p-0 text-blue-900 cursor-pointer"
+                variant="link"
+                onClick={() => {
+                  setShowFullText(!showFullText);
+                  setIsClamped(!isClamped);
+                }}
+              >
+                {!isClamped ? "Ver menos..." : "Ver más..."}
+              </Button>
+            }
+          </div>
 
           <div className="flex justify-start items-center gap-9">
-            <div className="flex items-center gap-2 text-neutral-500">
-              <MapPin className="size-5" />
-              <span className="text-sm truncate">
-                {userData.location}
-              </span>
-            </div>
+            {userData.location &&
+              <div className="flex items-center gap-2 text-neutral-500">
+                <MapPin className="size-5" />
+                <span className="text-sm truncate">
+                  {userData.location}
+                </span>
+              </div>
+            }
 
             <div className="flex items-center gap-2 text-neutral-500">
               <Calendar className="size-5" />

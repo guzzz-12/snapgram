ca85f6d007b54176d69d5d2333a996a29accbe78
unknown <jegq.dev.projects@gmail.com>
2025-10-12 19:12:26 -0400

    Funcionalidad para editar el post

--- src/components/posts/PostCard.tsx
+++ src/components/posts/PostCard.tsx
@@ -1,11 +1,18 @@
-import { useRef, type HTMLAttributes } from "react";
+import { useEffect, useRef, useState, type HTMLAttributes } from "react";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useAuth } from "@clerk/clerk-react";
 import Slider, {type Settings} from "react-slick";
+import { Twemoji } from "react-emoji-render";
+import { toast } from "sonner";
 import PostHeader from "./PostCardHeader";
 import PostCardFooter from "./PostCardFooter";
+import CreatePostInput from "./CreatePostInput";
 import { Button } from "../ui/button";
 import useClampedText from "@/hooks/useClampedText";
-import type { PostWithLikes } from "@/types/global";
+import { errorMessage } from "@/utils/errorMessage";
+import { axiosInstance } from "@/utils/axiosInstance";
 import { cn } from "@/lib/utils";
+import type { PostWithLikes } from "@/types/global";
 
 interface Props {
   postData: PostWithLikes;
@@ -26,6 +33,18 @@ const PostCard = ({ postData, isModal, className }: Props) => {
   const textContentRef = useRef<HTMLParagraphElement>(null);
   const showClampBtnRef = useRef<"shouldShow" | "shouldNotShow">("shouldNotShow");
 
+  const [isEditingPost, setIsEditingPost] = useState(false);
+  const [textContent, setTextContent] = useState(() => postData.content);
+
+  // Restablecer el texto inicial del post al salir
+  // del modo de edición sin guardar los cambios
+  useEffect(() => {
+    return () => setTextContent(postData.content);
+  }, [isEditingPost]);
+
+  const {getToken} = useAuth();
+  const queryClient = useQueryClient();
+
   const {
     isClamped,
     showFullText,
@@ -33,17 +52,84 @@ const PostCard = ({ postData, isModal, className }: Props) => {
     setIsClamped
   } = useClampedText({ textContentRef, showClampBtnRef });
 
+  const {mutate, isPending} = useMutation({
+    mutationFn: async () => {
+      const token = await getToken();
+
+      return axiosInstance({
+        method: "PUT",
+        url: `/posts/${postData._id}`,
+        data: {
+          content: textContent
+        },
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`
+        }
+      });
+    },
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({queryKey: ["posts"]});
+      setIsEditingPost(false);
+    },
+    onError: (error) => {
+      const message = errorMessage(error);
+      toast.error(message);
+      setTextContent(postData.content);
+    }
+  });
+
   return (
     <article className={cn("flex flex-col gap-3 w-full p-4 rounded-lg bg-white shadow", className)}>
-      <PostHeader postData={postData} />
+      <PostHeader
+        postData={postData}
+        setisEditingPost={(bool) => setIsEditingPost(bool)}
+      />
 
-      {postData.content && (
+      {isEditingPost && (
+        <div className="w-full">
+          <CreatePostInput
+            textContent={textContent}
+            isPending={isPending}
+            setTextContent={setTextContent}
+          />
+
+          <div className="flex justify-end items-center gap-2 w-full py-2">
+            <Button
+              className="cursor-pointer"
+              variant="outline"
+              disabled={isPending}
+              onClick={() => setIsEditingPost(false)}
+            >
+              Cancelar
+            </Button>
+
+            <Button
+              className="bg-[#4F39F6] hover:bg-[#331fcf] cursor-pointer disabled:pointer-events-none"
+              disabled={
+                isPending
+                ||
+                (textContent === postData.content)
+                ||
+                (!textContent && postData.postType === "text")
+              }
+              onClick={() => mutate()}
+            >
+              Guardar cambios
+            </Button>
+          </div>
+        </div>
+      )}
+
+      {postData.content && !isEditingPost && (
         <div>
           <p
             ref={textContentRef}
             className={cn("inline-block text-base text-left text-neutral-700 whitespace-pre-wrap bg-transparent", showFullText ? "line-clamp-none" : "line-clamp-6")}
           >
-            {postData.content}
+            <Twemoji className="[&>img]:!inline">
+              {postData.content}
+            </Twemoji>
           </p>
           
           {showClampBtnRef.current === "shouldShow" &&

7cb0ca75456dc6455e5f5a626c10109e9c23e46a
unknown <jegq.dev.projects@gmail.com>
2025-10-18 22:31:19 -0400

    Lista de resultados de la búsqueda de usuarios

---
+++ src/components/discover/UsersSearchResults.tsx
@@ -0,0 +1,129 @@
+import { useEffect, useRef, type RefObject } from "react";
+import { useAuth } from "@clerk/clerk-react";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import { toast } from "sonner";
+import SearchUserCardSkeleton from "./SearchUserCardSkeleton";
+import ResultUserCard from "./ResultUserCard";
+import NoResults from "./NoResults";
+import { axiosInstance } from "@/utils/axiosInstance";
+import { errorMessage } from "@/utils/errorMessage";
+import useIntersectionObserver from "@/hooks/useIntersectionObserver";
+import type { SearchUsersResult } from "@/types/global";
+
+interface Props {
+  searchTerm: string | null;
+  filter: "users" | "posts";
+  setTerm: (term: string) => void;
+  searchInputRef: RefObject<HTMLInputElement | null>;
+  setIsSearchingUsers: (isSearchingUsers: boolean) => void;
+}
+
+const UsersSearchResults = (props: Props) => {
+  const {
+    searchTerm,
+    filter,
+    setTerm,
+    searchInputRef,
+    setIsSearchingUsers
+  } = props;
+
+  const paginationRef = useRef<HTMLDivElement | null>(null);
+
+  const { getToken } = useAuth();
+
+  const searchUsers = async (page: number, keyword: string | null | undefined) => {
+    setIsSearchingUsers(true);
+
+    const token = await getToken();
+
+    const {data} = await axiosInstance<{
+      data: SearchUsersResult[];
+      hasMore: boolean;
+      nextPage: number | null;
+    }>({
+      method: "GET",
+      url: "/search/search-users",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      params: {
+        page,
+        keyword,
+        limit: 10
+      }
+    });
+
+    setIsSearchingUsers(false);
+
+    return data;
+  }
+
+  const {
+    data,
+    error,
+    isLoading,
+    isFetchingNextPage,
+    fetchNextPage
+  } = useInfiniteQuery({
+    queryKey: ["search", searchTerm, "users"],
+    queryFn: ({pageParam}) => searchUsers(pageParam, searchTerm),
+    initialPageParam: 1,
+    getNextPageParam: (lastPage) => lastPage.hasMore ? lastPage.nextPage : null,
+    refetchOnWindowFocus: false,
+    retry: 2,
+    enabled: !!searchTerm && filter === "users"
+  });
+
+  const {isIntersecting} = useIntersectionObserver({data, paginationRef});
+
+  useEffect(() => {
+    if (isIntersecting) {
+      fetchNextPage();
+    }
+  }, [isIntersecting]);
+
+  if (error) {
+    const message = errorMessage(error);
+    toast.error(message);
+  }
+
+  const searchUsersResults = data?.pages.flatMap(page => page.data) || [];
+
+  if (filter !== "users") return null;
+
+  return (
+    <>
+      <div className="grid grid-cols-1 min-[600px]:grid-cols-2 min-[768px]:grid-cols-1 min-[920px]:grid-cols-2 min-[1100px]:grid-cols-3 gap-4 w-full">
+        {isLoading && [...Array(6)].map((_, index) => (
+          <SearchUserCardSkeleton key={index} />
+        ))}
+
+        {!isLoading && searchUsersResults.map(user => {
+          return (
+            <ResultUserCard
+              key={user._id}
+              userData={user}
+            />
+          )
+        })}
+
+        {isFetchingNextPage && [...Array(6)].map((_, index) => (
+          <SearchUserCardSkeleton key={index} />
+        ))}
+
+        <div ref={paginationRef} className="w-full h-10" />
+      </div>
+
+      {searchTerm && !isLoading && searchUsersResults.length === 0 &&
+        <NoResults
+          term={searchTerm}
+          searchType={filter}
+          setTerm={(term) => setTerm(term)}
+          searchInputRef={searchInputRef}
+        />
+      }
+    </>
+  )
+}
+
+export default UsersSearchResults

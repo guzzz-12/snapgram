396123a84c1d342d0325f73798508e0c6bbfa45e
unknown <jegq.dev.projects@gmail.com>
2025-10-25 19:58:44 -0400

    Tabs y funcionalidad para filtrar las notificaciones por 'todas' y 'no leídas'

--- src/pages/NotificationsPage.tsx
+++ src/pages/NotificationsPage.tsx
@@ -1,10 +1,11 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { useAuth } from "@clerk/clerk-react";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { toast } from "sonner";
 import RightSidebar from "@/components/RightSidebar";
 import NotificationsList from "@/components/notifications/NotificationsList";
 import { SidebarTrigger } from "@/components/ui/sidebar";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import useIntersectionObserver from "@/hooks/useIntersectionObserver";
 import { axiosInstance } from "@/utils/axiosInstance";
 import { errorMessage } from "@/utils/errorMessage";
@@ -13,9 +14,11 @@ import type { NotificationType } from "@/types/global";
 const ConnectionsPage = () => {
   const paginationRef = useRef<HTMLDivElement>(null);
 
+  const [activeTab, setActiveTab] = useState<"all" | "unread">("all");
+
   const {getToken} = useAuth();
 
-  const getNotifications = async (page: number) => {
+  const getNotifications = async (page: number, activeTab: "all" | "unread") => {
     const token = await getToken();
 
     const {data} = await axiosInstance<{
@@ -27,10 +30,12 @@ const ConnectionsPage = () => {
       url: "/notifications",
       params: {
         page,
-        limit: 5
+        limit: 5,
+        filter: activeTab
       },
       headers: {
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json"
       }
     });
 
@@ -38,10 +43,11 @@ const ConnectionsPage = () => {
   }
 
   const {data, error, isLoading, isFetchingNextPage, hasNextPage, fetchNextPage} = useInfiniteQuery({
-    queryKey: ["notifications"],
-    queryFn: async ({pageParam}) => getNotifications(pageParam),
+    queryKey: ["notifications", activeTab],
+    queryFn: async ({pageParam}) => getNotifications(pageParam, activeTab),
     initialPageParam: 1,
-    getNextPageParam: (lastPage) => lastPage.hasMore ? lastPage.nextPage : null
+    getNextPageParam: (lastPage) => lastPage.hasMore ? lastPage.nextPage : null,
+    refetchOnWindowFocus: false
   });
 
   const {isIntersecting} = useIntersectionObserver({data, paginationRef});
@@ -62,8 +68,8 @@ const ConnectionsPage = () => {
     <main className="pageWrapper gap-10">
       <SidebarTrigger className="absolute block md:hidden top-4 left-4 cursor-pointer z-10" />
 
-      <section className="flex flex-col gap-6 w-full max-w-[900px] mx-auto">
-        <div className="">
+      <section className="flex flex-col gap-0 w-full max-w-[900px] mx-auto">
+        <div className="mb-6">
           <h1 className="text-2xl font-semibold">
             Notificaciones
           </h1>
@@ -72,6 +78,31 @@ const ConnectionsPage = () => {
             Administra tus notificaciones
           </p>
         </div>
+
+        <Tabs
+          className="w-fit mb-3 rounded-sm"
+          value={activeTab}
+          defaultValue="all"
+          onValueChange={(value) => {
+            setActiveTab(value as "all" | "unread");
+          }}
+        >
+          <TabsList className="w-full h-[50px] gap-2 bg-white rounded-md">
+            <TabsTrigger
+              className="w-[90px] !text-[16px] !text-center font-normal bg-white rounded-t-xs rounded-b-none border-t-0 border-l-0 border-r-0 border-b-3 border-transparent cursor-pointer transition-all data-[state=active]:text-[#4F39F6] data-[state=active]:font-semibold data-[state=active]:border-[#4F39F6] data-[state=active]:shadow-none data-[state=active]:bg-[#4F39F6]/10"
+              value="all"
+            >
+              Todas
+            </TabsTrigger>
+
+            <TabsTrigger
+              className="w-[90px] !text-[16px] !text-center font-normal bg-white rounded-t-xs rounded-b-none border-t-0 border-l-0 border-r-0 border-b-3 border-transparent cursor-pointer transition-all data-[state=active]:text-[#4F39F6] data-[state=active]:font-semibold data-[state=active]:border-[#4F39F6] data-[state=active]:shadow-none data-[state=active]:bg-[#4F39F6]/10"
+              value="unread"
+            >
+              No leídas
+            </TabsTrigger>
+          </TabsList>
+        </Tabs>
 
         <NotificationsList
           notifications={notifications}

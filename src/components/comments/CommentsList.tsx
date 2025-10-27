3e93ef33b24e85373e8d1cd98e8847670522e488
unknown <jegq.dev.projects@gmail.com>
2025-10-27 19:28:39 -0400

    No mostrar el texto placeholder de la lista de replies que se mostraba cuando no hay replies

--- src/components/comments/CommentsList.tsx
+++ src/components/comments/CommentsList.tsx
@@ -12,29 +12,33 @@ interface Props {
 }
 
 const CommentsList = ({ comments, isLoading, className, isReply }: Props) => {
-  if (comments.length === 0 && !isLoading) {
+  if (comments.length === 0 && !isLoading && !isReply) {
     return (
       <div className="flex flex-col justify-center items-center w-full py-5">
         <MessageSquareText className="w-[150px] h-[150px] text-neutral-500 stroke-[0.75]" />
-        <p className={cn("text-neutral-500 font-semibold", isReply ? "text-sm" : "text-base")}>
-          {isReply ? "Sé el primero en responder" : "Todavía no hay comentarios"}
+        <p className="text-neutral-500 font-semibold text-base">
+          Todavía no hay comentarios
         </p>
       </div>
     )
   };
 
-  return (
-    <ul className={cn("flex flex-col gap-4 py-3", className)}>
-      {comments.map(comment => {
-        return (
-          <CommentItem
-            key={comment._id}
-            commentData={comment}
-          />
-        )
-      })}
-    </ul>
-  )
+  if (comments.length > 0) {
+    return (
+      <ul className={cn("flex flex-col gap-4 py-3", className)}>
+        {comments.map(comment => {
+          return (
+            <CommentItem
+              key={comment._id}
+              commentData={comment}
+            />
+          )
+        })}
+      </ul>
+    )
+  }
+
+  return null;
 }
 
 export default CommentsList

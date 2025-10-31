d15ca1e16a8e5d88f9ad1e14570305c7356a0649
unknown <jegq.dev.projects@gmail.com>
2025-10-30 23:15:40 -0400

    Función para incrementar por 1 el contador de notificaciones sin leer

--- src/hooks/useUnseenNotifications.ts
+++ src/hooks/useUnseenNotifications.ts
@@ -2,10 +2,14 @@ import { create } from "zustand";
 
 interface State {
   unseenNotifications: number;
-  setUnseenNotifications: (count: number) => void
+  setUnseenNotifications: (count: number) => void;
+  increaseNotificationsCount: () => void;
 }
 
 export const useUnseenNotifications = create<State>((set) => ({
   unseenNotifications: 0,
   setUnseenNotifications: (count) => set({ unseenNotifications: count }),
+  increaseNotificationsCount: () => set((state) => {
+    return { unseenNotifications: state.unseenNotifications + 1 }
+  })
 }));

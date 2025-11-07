2fb71a2ccd6ed362f551cfb6e035956a9f8cf311
unknown <jegq.dev.projects@gmail.com>
2025-11-06 23:16:57 -0400

    Refactorización del custom hook que actualiza la caché de los mensajes y de la lista de chats

--- src/hooks/useNewMessage.ts
+++ src/hooks/useNewMessage.ts
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
-import { useQueryClient, type InfiniteData } from "@tanstack/react-query";
+import { useQueryClient } from "@tanstack/react-query";
 import { socket } from "@/utils/socket";
-import type { ChatType, MessageType } from "@/types/global";
+import { updateChatsListCache, updateMessagesCache } from "@/utils/updateMsgsDataCache";
 
 interface Props {
   chatId: string;
   currentUserId: string;
 }
 
-/** Escuchar el evento de nuevo mesaje y actualizar
- * la bandeja de entrada del chat en tiempo real
-  */
+/** Escuchar el evento de nuevo mesaje para actualizar en tiempo real la bandeja
+ * de entrada y agregar el item a la lista de chats si es un chat nuevo
+*/
 export const useNewMessage = (props: Props) => {
   const { chatId, currentUserId } = props;
 
@@ -20,82 +20,12 @@ export const useNewMessage = (props: Props) => {
     socket.on("newPrivateMessage", (newMessage) => {
       if (newMessage.chat._id === chatId) {
         // Actualizar la caché de los mensajes para actualizar la bandeja de entrada del chat
-        queryClient.setQueryData(["messages", chatId], (oldData: InfiniteData<{
-          data: {
-            messages: MessageType[];
-            chat: ChatType;
-            isNewChat: boolean;
-          };
-          hasMore: boolean;
-          nextPage: number | null;
-        }>) => {
-          if (!oldData) {
-            return oldData;
-          }
-
-          // La data de useInfiniteQuery tiene la forma { pages: [...], pageParams: [...] }
-          const newData = oldData.pages.map((page, index) => {
-            if (index === 0) {
-              const currentMessages = page.data.messages;
-
-              return {
-                ...page,
-                data: {
-                  messages: [newMessage.message, ...currentMessages],
-                  chat: newMessage.chat,
-                  isNewChat: newMessage.isNewChat
-                }
-              };
-            }
-
-            // Retornar las otras páginas sin cambios
-            return page;
-          });
-
-          // Retornar la caché actualizada
-          return {
-            ...oldData,
-            pages: newData,
-          };
-        });
+        updateMessagesCache({ queryClient, chatId, newMessage });
       }
 
       // Actualizar la caché de la lista de chats para agregar
       // el nuevo chat a la lista de chats del usuario recipiente
-      const isNewChat = newMessage.isNewChat;
-      const isRecipient = newMessage.message.sender._id !== currentUserId;
-
-      if (isNewChat && isRecipient) {
-        queryClient.setQueryData(["chats"], (oldData: InfiniteData<{
-          data: ChatType[];
-          hasMore: boolean;
-          nextPage: number | null;
-        }>) => {
-          if (!oldData) {
-            return oldData;
-          }
-
-          const newData = oldData.pages.map((page, index) => {
-            if (index === 0) {
-              const currentChats = page.data || [];
-
-              return {
-                ...page,
-                data: [newMessage.chat, ...currentChats]
-              };
-            }
-
-            // Retornar las otras páginas sin cambios
-            return page;
-          });
-
-          // Retornar la caché actualizada
-          return {
-            ...oldData,
-            pages: newData,
-          };
-        });
-      }
+      updateChatsListCache({ queryClient, newMessage, currentUserId });
     });
 
     return () => {

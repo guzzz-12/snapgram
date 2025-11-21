68eee30bb9ab51ef2bd3899d5b62f785b0d170d7
unknown <jegq.dev.projects@gmail.com>
2025-11-21 00:13:17 -0400

    Si el chat es privado, mostrar en el header el nombre del otro usuario, sino, mostrar el nombre del grupo

--- src/components/chat/ChatHeader.tsx
+++ src/components/chat/ChatHeader.tsx
@@ -1,14 +1,25 @@
 import type { RefObject } from "react";
 import { Link } from "react-router";
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import type { UserType } from "@/types/global";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useCurrentUser } from "@/hooks/useCurrentUser";
+import type { ChatType } from "@/types/global";
 
 interface Props {
-  recipient: UserType;
+  chatData: ChatType | null;
   headerRef: RefObject<HTMLDivElement | null>;
 }
 
-const ChatHeader = ({ recipient, headerRef }: Props) => {
+const ChatHeader = ({ chatData, headerRef }: Props) => {
+  const {user: currentUser} = useCurrentUser();
+
+  const chatParticipants = chatData?.participants || [];
+
+  // Si el chat es privado, mostrar el avatar del otro usuario
+  // Si el chat es de grupo, mostrar el avatar del admin del grupo
+  const recipient = chatData?.type === "private" ? chatParticipants.find((p) => p._id !== currentUser?._id) : chatData?.groupAdmin;
+
+  if (!recipient) return null;
+
   return (
     <div
       ref={headerRef}
@@ -22,16 +33,24 @@ const ChatHeader = ({ recipient, headerRef }: Props) => {
           <AvatarImage 
             className="w-full h-full object-cover" 
             src={recipient.profilePicture} 
+            // src={chatData?.type === "group" ? chatData.groupPicture : recipient.profilePicture} 
           />
           
           <AvatarFallback>
-            {recipient.fullName.charAt(0).toUpperCase()}
+            {chatData?.type === "group" ? "👥" : recipient.fullName.charAt(0).toUpperCase()}
           </AvatarFallback>
         </Avatar>
 
         <div className="flex flex-col justify-between items-start gap-0 w-full">
-          <p className="font-semibold text-neutral-900">{recipient.fullName}</p>
-          <p className="text-sm text-neutral-700">@{recipient.username}</p>
+          <p className="font-semibold text-neutral-900">
+            {chatData?.type === "group" ? chatData.groupName : recipient.fullName}
+          </p>  
+
+          {chatData?.type !== "group" &&
+            <p className="text-sm text-neutral-700">
+              @{recipient.username}
+            </p>
+          }
         </div>
       </Link>
     </div>

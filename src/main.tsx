ded5e323d1c0e09f05491e13dce912d631342206
unknown <jegq.dev.projects@gmail.com>
2025-12-17 15:40:00 -0400

    Implementación de error boundary

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,9 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ClerkProvider } from "@clerk/clerk-react";
 import { esMX } from "@clerk/localizations";
 import { ImageKitProvider } from "@imagekit/react";
+import { ErrorBoundary } from "react-error-boundary";
 import App from "./App.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "yet-another-react-lightbox/styles.css";
@@ -18,17 +20,34 @@ if (!PUBLISHABLE_KEY) {
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
-  <QueryClientProvider client={queryClient}>
-    <ClerkProvider
-      publishableKey={PUBLISHABLE_KEY}
-      localization={esMX}
-    >
-      <BrowserRouter>
-        <ImageKitProvider urlEndpoint="https://ik.imagekit.io/y1lpjbueh/">
-          <App />
-        </ImageKitProvider>
-      </BrowserRouter>
-    </ClerkProvider>
-  </QueryClientProvider>
-);
+const Fallback = ({error}: {error: Error}) => {
+  console.log(error);
+
+  return (
+    <ErrorPage
+      title="¡Oops! Algo salió mal."
+      message="Actualiza la página e inténtalo de nuevo."
+    />
+  )
+}
+
+const MyApp = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ClerkProvider
+        publishableKey={PUBLISHABLE_KEY}
+        localization={esMX}
+      >
+        <BrowserRouter>
+          <ImageKitProvider urlEndpoint="https://ik.imagekit.io/y1lpjbueh/">
+            <ErrorBoundary FallbackComponent={Fallback}>
+              <App />
+            </ErrorBoundary>
+          </ImageKitProvider>
+        </BrowserRouter>
+      </ClerkProvider>
+    </QueryClientProvider>
+  )
+}
+
+createRoot(document.getElementById("root")!).render(<MyApp />);

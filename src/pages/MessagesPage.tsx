8dd469943c5df78e7871b5e585c395c9131168a0
unknown <jegq.dev.projects@gmail.com>
2025-12-28 21:39:19 -0400

    Mostrar spinner en el chat mientras se verifica si el usuario tiene claves de cifrado

--- src/pages/MessagesPage.tsx
+++ src/pages/MessagesPage.tsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { useParams, useSearchParams } from "react-router";
 import { LuSendHorizontal  } from "react-icons/lu";
+import { Loader2Icon } from "lucide-react";
 import ChatList from "@/components/chat/ChatList";
 import ChatInbox from "@/components/chat/ChatInbox";
 import PrivateChatsModalList from "@/components/chat/PrivateChatsModalList";
 import CreateGroupChatModal from "@/components/chat/CreateGroupChatModal";
+import GetCryptoKeysModal from "@/components/chat/GetCryptoKeysModal";
 import { Button } from "@/components/ui/button";
+import { useCheckCryptoKeys } from "@/providers/CheckCryptoKeysProvider";
 import { usePrivateChatsListModal } from "@/hooks/usePrivateChatsListModal";
 import type { ChatType } from "@/types/global";
 
@@ -19,14 +22,25 @@ const MessagesPage = () => {
 
   const {setIsOpen: setOpenPrivateChatsModal} = usePrivateChatsListModal();
 
+  const {hasCryptoKeys, loadingPrivateKey} = useCheckCryptoKeys();
+
   return (
-    <main className="flex w-full h-full bg-white overflow-hidden">
+    <main className="relative flex w-full h-full bg-white overflow-hidden">
+      {loadingPrivateKey &&
+        <div className="absolute top-0 left-0 flex justify-center items-center w-full h-full bg-white/60 z-10">
+          <Loader2Icon className="w-10 h-10 animate-spin text-[#4F39F6]" />
+        </div>
+      }
+
+      <GetCryptoKeysModal />
+
       <PrivateChatsModalList setTemporaryChat={setTemporaryChat}/>
       
       <ChatList
         headerHeight={headerHeight}
         temporaryChatItem={temporaryChat}
         chatTypeParam={chatTypeParam}
+        hasCryptoKeys={hasCryptoKeys}
       />
 
       <section className="flex flex-col justify-start w-full h-screen overflow-x-hidden">
@@ -39,7 +53,7 @@ const MessagesPage = () => {
           />
         }
 
-        {!chatId &&
+        {!chatId && hasCryptoKeys &&
           <div className="flex flex-col justify-center items-center w-full h-full">
             <div className="flex justify-center items-center w-[120px] h-[120px] mb-2 shrink-0 rounded-full border-2 border-neutral-600">
               <LuSendHorizontal className="size-16 text-neutral-600 stroke-1" />

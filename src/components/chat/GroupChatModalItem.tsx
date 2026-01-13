59df8029ddd2947f284d0f33dab5202f2c74c16c
unknown <jegq.dev.projects@gmail.com>
2026-01-13 09:23:27 -0400

    Impedir agregar un miembro al grupo si no tiene claves de cifrado

--- src/components/chat/GroupChatModalItem.tsx
+++ src/components/chat/GroupChatModalItem.tsx
@@ -4,6 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Checkbox } from "@/components/ui/checkbox";
 import type { UserType } from "@/types/global";
 import { cn } from "@/lib/utils";
+import { Tooltip, TooltipContent, TooltipTrigger } from "@/components/ui/tooltip";
 
 interface Props {
   userData: UserType;
@@ -21,35 +22,46 @@ const GroupChatModalItem = ({ userData, className, setSelectedUsersIds }: Props)
   };
 
   return (
-    <Label
-      id={userData._id}
-      className={cn("flex justify-start items-center gap-4 p-2 rounded-md bg-transparent hover:bg-[#4F39F6]/10 cursor-pointer", className)}
-    >
-      <div className="flex justify-start items-center gap-2 w-full">
-        <Avatar className="w-[45px] h-[45px] shrink-0">
-          <AvatarImage
-            className="w-full h-full object-cover"
-            src={userData.profilePicture || "/default_avatar.webp"}
-          />
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Label
+          id={userData._id}
+          className={cn("flex justify-start items-center gap-4 p-2 rounded-md bg-transparent hover:bg-[#4F39F6]/10", userData.hasCryptoKeys ? "cursor-pointer" : "cursor-not-allowed", className)}
+        >
+          <div className="flex justify-start items-center gap-2 w-full">
+            <Avatar className="w-[45px] h-[45px] shrink-0">
+              <AvatarImage
+                className="w-full h-full object-cover"
+                src={userData.profilePicture || "/default_avatar.webp"}
+              />
+              <AvatarFallback className="w-full h-full object-cover">
+                {userData.fullName.charAt(0).toUpperCase()}
+              </AvatarFallback>
+            </Avatar>
+            <div className="flex flex-col justify-center items-start gap-1 w-full">
+              <p className="font-semibold text-neutral-900">{userData.fullName}</p>
+              <p className="text-xs text-neutral-600">@{userData.username}</p>
+            </div>
+          </div>
+          {userData.hasCryptoKeys &&
+            <Checkbox
+              id={userData._id}
+              className="size-6.5 start-0 rounded-full cursor-pointer data-[state=checked]:border-[#4F39F6] data-[state=checked]:bg-[#4F39F6] data-[state=checked]:text-white"
+              value={userData._id}
+              onCheckedChange={onCheckedChange}
+            />
+          }
+        </Label>
+      </TooltipTrigger>
 
-          <AvatarFallback className="w-full h-full object-cover">
-            {userData.fullName.charAt(0).toUpperCase()}
-          </AvatarFallback>
-        </Avatar>
-
-        <div className="flex flex-col justify-center items-start gap-1 w-full">
-          <p className="font-semibold text-neutral-900">{userData.fullName}</p>
-          <p className="text-xs text-neutral-600">@{userData.username}</p>
-        </div>
-      </div>
-
-      <Checkbox
-        id={userData._id}
-        className="size-6.5 start-0 rounded-full cursor-pointer data-[state=checked]:border-[#4F39F6] data-[state=checked]:bg-[#4F39F6] data-[state=checked]:text-white"
-        value={userData._id}
-        onCheckedChange={onCheckedChange}
-      />
-    </Label>
+      {!userData.hasCryptoKeys &&
+        <TooltipContent>
+          <span className="block text-center">
+            {userData.fullName.split(" ")[0]} no ha habilitado <br /> el cifrado del chat.
+          </span>
+        </TooltipContent>
+      }
+    </Tooltip>
   )
 }
 

65a08daacae0aefd79113f632c8c942b097406df
unknown <jegq.dev.projects@gmail.com>
2026-03-19 00:26:22 -0400

    Props del logo principal para especificar el theme y el tamaño

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -4,23 +4,25 @@ import logo from "@/assets/logo-simple.webp";
 import { cn } from "@/lib/utils";
 
 interface Props {
+  size?: "sm" | "md" | "lg";
+  mode?: "light" | "dark";
   className?: HTMLAttributes<HTMLElement>["className"];
 }
 
-const Logo = ({ className }: Props) => {
+const Logo = ({ size = "md", mode = "light", className }: Props) => {
   return (
     <Link
       className={cn("flex justify-center items-center gap-2 w-fit", className)}
       to="/"
     >
       <img
-        className="w-[50px] h-auto"
+        className={cn("h-auto", size === "sm" ? "w-[35px]" : size === "lg" ? "w-[75px]" : "w-[50px]")}
         src={logo}
         alt="logo"
         aria-hidden
       />
 
-      <h1 className="text-2xl text-neutral-900 font-semibold">
+      <h1 className={cn("text-2xl font-semibold", mode === "light" ? "text-neutral-900" : "text-white")}>
         SnapGram
       </h1>
     </Link>

30ce40624502cabd73b3ffda42f29f64ce6745c2
unknown <jegq.dev.projects@gmail.com>
2026-03-19 00:27:42 -0400

    Los items de los usuarios que vieron la historia son links a sus perfiles respectivos

--- src/components/stories/SeenByUsers.tsx
+++ src/components/stories/SeenByUsers.tsx
@@ -1,6 +1,7 @@
-import type { UserType } from "@/types/global"
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import type { HTMLAttributes } from "react";
+import { Link } from "react-router";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import type { UserType } from "@/types/global";
 import { cn } from "@/lib/utils";
 
 interface Props {
@@ -21,9 +22,10 @@ const SeenByUsers = ({ data, className }: Props) => {
     <div className={cn("flex flex-col justify-center items-center", className)}>
       <div className="flex justify-center items-center gap-0">
         {data.slice(0, sliceLength).map((el, i) => (
-          <div
+          <Link
             key={el.user._id}
             className={cn("flex justify-start items-center gap-0")}
+            to={`/profile/${el.user.clerkId}`}
           >
             <Avatar className="w-[24px] h-[24px] shrink-0">
               <AvatarImage
@@ -34,19 +36,19 @@ const SeenByUsers = ({ data, className }: Props) => {
                 {el.user.fullName.charAt(0).toUpperCase()}
               </AvatarFallback>
             </Avatar>
-          </div>
+          </Link>
         ))}
 
         {totalViewers > sliceLength && (
           <div className="flex justify-center items-center gap-0 min-w-[20px] h-[20px] ml-0.5 shrink-0">
-            <span className="block px-1 text-xs text-neutral-300">
+            <span className="block px-1 text-[10px] text-neutral-300">
               +{totalViewers - sliceLength}
             </span>
           </div>
         )}
       </div>
 
-      <p className="mt-1 text-center text-xs text-neutral-300 font-light">
+      <p className="mt-1 text-center text-[10px] text-neutral-300 font-light">
         Vista por {totalViewers}
       </p>
     </div>
